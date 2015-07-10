3867fed4792c651ac92ed07ed785c3c71f45a973
Adam Heller <aheller@persistentsystems.com>
2015-07-10 16:35:24 -0400

    works on more websites

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,75 @@
-console.log("Hello world extension");
 
-$("#commenting").hide();
+console.log("extension loaded");
+
+// hideThemAll();
+
+// $(document).ready( function() {
+
+// 	hideThemAll();
+
+// });
+
+$(window).load(function() {
+
+	hideThemAll();
+
+});
+
+
+
+// $(window).onLoad( function() {
+
+// 	console.log("window load loaded");
+
+// 	hideThemAll();
+
+// 	return 0;
+
+// });
+
+
+function hideThemAll() { 
+
+	console.log("hideThemAll executing");
+
+	// while(true) {
+
+	try {
+
+		$("#commenting").css("display", "none");
+
+	} catch (e) { }
+	
+	try {
+
+		$(".comments-view").hide();
+
+	} catch (e) { }
+
+	try {
+
+		$("#disqus_thread").hide();
+
+	} catch(e) { }
+
+	try {
+
+		$("#showComments").hide();
+
+	} catch(e) { }
+
+
+	// $("#comments-view").hide();
+
+	// $(".comments-marginalia").hide();
+
+	
+
+	// $(".comment").hide();
+
+	// $("#comment").hide();
+
+	// }
+	
+
+}
